40735aa68969b143d9fc7a2d0663fd754bc37809
semproms <semproms5@proton.me>
2025-11-07 22:37:12 +0100

    Basic structure project

---
+++ server.ts
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Sample route
+app.get('/', (req: any, res: any) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
